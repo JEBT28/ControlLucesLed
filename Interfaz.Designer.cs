e260bab48e19f1d076fb20ed6018b07def4c0dbc
Juan Tobon <juanbal2808@gmail.com>
2021-11-27 17:26:00 -0600

    Primer commit

---
+++ Interfaz.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace Proyecto_Arduino
+{
+    partial class Interfaz
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Interfaz));
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.cmbPuertosSeriales = new System.Windows.Forms.ToolStripComboBox();
+            this.btnConectar = new System.Windows.Forms.ToolStripButton();
+            this.btnCargarSP = new System.Windows.Forms.ToolStripButton();
+            this.toolStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.btnCargarSP,
+            this.cmbPuertosSeriales,
+            this.btnConectar});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(800, 25);
+            this.toolStrip1.TabIndex = 0;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // cmbPuertosSeriales
+            // 
+            this.cmbPuertosSeriales.Name = "cmbPuertosSeriales";
+            this.cmbPuertosSeriales.Size = new System.Drawing.Size(121, 25);
+            // 
+            // btnConectar
+            // 
+            this.btnConectar.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.btnConectar.Image = ((System.Drawing.Image)(resources.GetObject("btnConectar.Image")));
+            this.btnConectar.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.btnConectar.Name = "btnConectar";
+            this.btnConectar.Size = new System.Drawing.Size(59, 22);
+            this.btnConectar.Text = "Conectar";
+            // 
+            // btnCargarSP
+            // 
+            this.btnCargarSP.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.btnCargarSP.Image = ((System.Drawing.Image)(resources.GetObject("btnCargarSP.Image")));
+            this.btnCargarSP.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.btnCargarSP.Name = "btnCargarSP";
+            this.btnCargarSP.Size = new System.Drawing.Size(89, 22);
+            this.btnCargarSP.Text = "Cargar Puertos";
+            this.btnCargarSP.Click += new System.EventHandler(this.btnCargarSP_Click);
+            // 
+            // Interfaz
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.toolStrip1);
+            this.Name = "Interfaz";
+            this.Text = "Interfaz";
+            this.Load += new System.EventHandler(this.Interfaz_Load);
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripComboBox cmbPuertosSeriales;
+        private System.Windows.Forms.ToolStripButton btnConectar;
+        private System.Windows.Forms.ToolStripButton btnCargarSP;
+    }
+}
---
+++ Interfaz.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Proyecto_Arduino
+{
+    public partial class Interfaz : Form
+    {
+        private SerialPort puertoComunicacion;
+
+        public Interfaz()
+        {
+            InitializeComponent();
+        }
+
+        private void Interfaz_Load(object sender, EventArgs e)
+        {
+          
+        }
+
+        private void LeerPuertosSeriales()
+        {
+            cmbPuertosSeriales.Items.Clear();
+            Debug.WriteLine(String.Join(" ,",SerialPort.GetPortNames()));
+            cmbPuertosSeriales.Items.AddRange(SerialPort.GetPortNames());
+        }
+
+        private void btnCargarSP_Click(object sender, EventArgs e)
+        {
+            LeerPuertosSeriales();
+        }
+
+        private void Conectar(string puerto)
+        {
+            puertoComunicacion = new SerialPort();
+
+            puertoComunicacion.BaudRate = 9600;
+            puertoComunicacion.PortName = puerto;
+
+            puertoComunicacion.DataReceived += new SerialDataReceivedEventHandler(puertoComunicacion_DataReceived);
+
+        }
+
+        private void puertoComunicacion_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            // Obtenemos el puerto serie que lanza el evento
+            SerialPort currentSerialPort = (SerialPort)sender;
+
+            // Leemos el dato recibido del puerto serie
+            Debug.WriteLine($"Dato recibido: {currentSerialPort.ReadLine()}");
+        }
+    }
+}
